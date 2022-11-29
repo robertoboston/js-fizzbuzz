84e673535bad7a51697329d04972d5747b6b96e2
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-29 16:31:34 +0100

    fizz buzz dom created

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+const container=document.getElementById('container');
 
 for(let i = 1; i <= 100; i++ ){
+    let element;
+
     if((i % 3==0) && (i % 5==0)){
         console.log('FizzBuzz')
+        element=`<div class="box">FizzBuzz</div>`
     }
     if(i % 3== 0){
         console.log('Fizz')
-        
+        element=`<div class="box box-green">Fizz</div>`
     }
     else if (i % 5== 0){
         console.log('Buzz')
+        element=`<div class="box box-black">Buzz</div>`
     }
     else{
         console.log(i)
+        element=`<div class="box box-blue">${i}</div>`
     }
+
+    container.innerHTML += element
+    
     
     
     
